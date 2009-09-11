64001de322fa18ce4398bb4570656c19272f2df0
Simon J Greenhill <simon@simon.net.nz>
2009-09-11 19:58:18 +1200

    update

---
+++ readme.txt
@@ -0,0 +1,14 @@
+
+
+Usage:
+
+>>> n = Nexus('examples/example.nex')
+>>> n.nexus.keys()
+['data']
+>>> n.nexus['data'].taxa
+['Harry', 'Simon', 'Betty', 'Louise']
+>>> n.nexus['data'].nchar
+2
+>>> n.nexus['data'].ntaxa
+4
+
---
+++ tests.py
@@ -0,0 +1,34 @@
+"""Tests for nexus reading"""
+import nose
+from nexus import *
+
+class Test_DataHandler_SimpleStandardNexus:
+    def setup(self):
+        self.nex = Nexus('examples/example.nex')
+        
+    def test_block_find(self):
+        assert self.nex.blocks.keys() == ['data']
+        
+    def test_format_string(self):
+        #assert self.nex.blocks['data'].format['datatype'] == 'standard'
+        #assert self.nex.blocks['data'].format['gap'] == '-'
+        #assert self.nex.blocks['data'].format['symbols'] == '01'
+        pass
+        
+    def test_taxa(self):
+        assert 'Simon' in self.nex.blocks['data'].matrix.keys()
+        assert 'Harry' in self.nex.blocks['data'].matrix.keys()
+        assert 'Betty' in self.nex.blocks['data'].matrix.keys()
+        assert 'Louise' in self.nex.blocks['data'].matrix.keys()
+        assert self.nex.blocks['data'].taxa == ['Harry', 'Simon', 'Betty', 'Louise']
+        assert self.nex.blocks['data'].ntaxa == 4 == len(self.nex.blocks['data'].taxa)
+        
+    def test_characters(self):
+        assert self.nex.blocks['data'].nchar == 2
+        print self.nex.blocks['data'].matrix
+        assert self.nex.blocks['data'].matrix['Harry'] == ['00']
+        assert self.nex.blocks['data'].matrix['Simon'] == ['01']
+        assert self.nex.blocks['data'].matrix['Betty'] == ['10']
+        assert self.nex.blocks['data'].matrix['Louise'] == ['11']
+        
+    
