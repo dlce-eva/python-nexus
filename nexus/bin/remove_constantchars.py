bdd8652d85c54ba851ab55cbcfd46069682ae0c8
Simon J Greenhill <simon@simon.net.nz>
2009-12-18 23:02:08 +1300

    update

---
+++ nexus/bin/remove_constantchars.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+"""remove_constantchars - python-nexus tools 
+Removes the constant characters from a nexus
+"""
+__author__ = 'Simon Greenhill <simon@simon.net.nz>'
+
+import sys
+import os
+
+from nexus.reader import NexusReader, NexusFormatException
+from nexus.writer import NexusWriter
+
+def find_constant_sites(nexus_obj):
+    """
+    Returns a list of the constant sites in a nexus
+    
+    :param nexus_obj: A `NexusReader` instance
+    :type nexus_obj: NexusReader 
+    
+    :return: A list of constant site positions.
+    :raises AssertionError: if nexus_obj is not a nexus
+    :raises NexusFormatException: if nexus_obj does not have a `data` block
+    """
+    assert isinstance(nexus_obj, NexusReader), "Nexus_obj should be a NexusReader instance"
+    if hasattr(nexus_obj, 'data') == False:
+        raise NexusFormatException("Nexus has no `data` block")
+        
+    const = []
+    for i in range(0, nexus_obj.data.nchar):
+        states = []
+        for taxa, data in nexus_obj.data:
+            characters = "".join(data)
+            c = characters[i]
+            if c in ('?', '-'):
+                continue
+            elif c not in states:
+                states.append(c)
+        
+        if len(states) == 1:
+            const.append(i)
+    return const
+    
+    
+
+if __name__ == '__main__':
+    #set up command-line options
+    from optparse import OptionParser
+    parser = OptionParser(usage="usage: %prog fudge.nex")
+    options, args = parser.parse_args()
+    
+    try:
+        nexusname = args[0]
+    except IndexError:
+        print __doc__
+        print "Author: %s\n" % __author__
+        parser.print_help()
+        sys.exit()
+        
+    nexus = NexusReader(nexusname)
+    const = find_constant_sites(nexus)
+    print("%d constant sites found: %s" % (len(const), ",".join(["%s" % s for s in const])))
+    if len(const) == 0:
+        print("Nothing to do!")
+        quit()
+    
+    # make new nexus
+    nexout = NexusWriter()
+    nexout.add_comment("%d constant sites removed from %s" % (len(const), nexusname))
+    nexout.add_comment("Removed sites: %s" % ",".join(["%s" % s for s in const]))
+    
+    
---
+++ nexus/test/test_bin.py
@@ -0,0 +1,73 @@
+"""Tests for utils in bin directory"""
+import os
+import nose
+
+from nexus import NexusReader
+from nexus.bin.calc_missings import count_missings
+from nexus.bin.remove_constant_chars import find_constant_sites
+
+
+EXAMPLE_DIR = os.path.join(os.path.split(os.path.dirname(__file__))[0], 'examples')
+
+class Test_count_missings:
+    
+    @nose.tools.raises(AssertionError)
+    def test_failure_on_nonnexus1(self):
+        count_missings({})
+        
+    @nose.tools.raises(AssertionError)
+    def test_failure_on_nonnexus2(self):
+        count_missings("I AM NOT A NEXUS")
+        
+    def test_count_missings_one(self):
+        nexus = NexusReader(os.path.join(EXAMPLE_DIR, 'example.nex'))
+        missing = count_missings(nexus)
+        for taxon in missing:
+            assert missing[taxon] == 0
+    
+    def test_count_missings_two(self):
+        expected = {'Harry': 0, 'Simon': 1, 'Peter': 1, 'Betty': 2, 'Louise': 3}
+        nexus = NexusReader()
+        nexus.read_string("""#NEXUS 
+        Begin data;
+        Dimensions ntax=4 nchar=3;
+        Format datatype=standard symbols="01" gap=-;
+        Matrix
+        Harry              010  [No missing]
+        Simon              0?0  [one missing]
+        Peter              0-0  [one gap]
+        Betty              ?-1  [one gap and one missing = 2 missing]
+        Louise             ???  [three missing]
+            ;
+        End;
+        """)
+        missing = count_missings(nexus)
+        for taxon in missing:
+            assert missing[taxon] == expected[taxon]
+
+
+class Test_find_constant_sites:
+    
+    @nose.tools.raises(AssertionError)
+    def test_failure_on_nonnexus1(self):
+        find_constant_sites({})
+    
+    @nose.tools.raises(AssertionError)
+    def test_failure_on_nonnexus2(self):
+        find_constant_sites("I AM NOT A NEXUS")
+    
+    def test_find_constant_sites1(self):
+        nexus = NexusReader(os.path.join(EXAMPLE_DIR, 'example.nex'))
+        assert len(find_constant_sites(nexus)) == 0
+    
+    def test_find_constant_sites2(self):
+        nexus = NexusReader(os.path.join(EXAMPLE_DIR, 'example.nex'))
+        const = find_constant_sites(nexus)
+        assert len(const) == 4
+        assert 0 in const
+        assert 1 in const
+        assert 2 in const
+        assert 3 in const
+    
+
+
