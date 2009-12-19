4b1f8059c997c06ad23c5ff96279fb41969f3d24
Simon J Greenhill <simon@simon.net.nz>
2009-12-20 10:07:14 +1300

    update'

--- nexus/bin/remove_constantchars.py
+++ nexus/bin/remove_constantchars.py
@@ -40,16 +40,32 @@ def find_constant_sites(nexus_obj):
             const.append(i)
     return const
     
+def new_nexus_without_sites(nexus_obj, sites_to_remove):
+    # make new nexus
+    nexout = NexusWriter()
+    nexout.add_comment(
+        "Removed %d constant sites: %s" %
+        (len(sites_to_remove), ",".join(["%s" % s for s in sites_to_remove]))
+    )
+    new_sitepos = 0
+    for sitepos in range(nexus_obj.data.nchar):
+        if sitepos in sites_to_remove:
+            continue # skip!
+        for taxon, data in nexus.data:
+            nexout.add(taxon, new_sitepos, data[sitepos])
+        new_sitepos += 1
+    return nexout
     
 
 if __name__ == '__main__':
     #set up command-line options
     from optparse import OptionParser
-    parser = OptionParser(usage="usage: %prog fudge.nex")
+    parser = OptionParser(usage="usage: %prog fudge.nex newfile.nex")
     options, args = parser.parse_args()
     
     try:
         nexusname = args[0]
+        newfile = args[1]
     except IndexError:
         print __doc__
         print "Author: %s\n" % __author__
@@ -60,13 +76,9 @@ if __name__ == '__main__':
     const = find_constant_sites(nexus)
     print("%d constant sites found: %s" % (len(const), ",".join(["%s" % s for s in const])))
     if len(const) == 0:
-        print("Nothing to do!")
+        raise Exception("Nothing to do!")
         quit()
     
-    # make new nexus
-    nexout = NexusWriter()
-    nexout.add_comment("%d constant sites removed from %s" % (len(const), nexusname))
-    nexout.add_comment("Removed sites: %s" % ",".join(["%s" % s for s in const]))
+    new = new_nexus_without_sites(nexus, const)
+    new.write_to_file(filename=newfile)
     
-    for taxon in nexus.data:
-        print taxon
