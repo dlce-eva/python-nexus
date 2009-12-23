55d86f6f956399983bda453a2d7a89b671831177
Simon Greenhill <simon@simon.net.nz>
2009-12-24 11:41:39 +1300

    refactoring tests for writer

--- nexus/test/test_writer.py
+++ nexus/test/test_writer.py
@@ -43,6 +43,20 @@ class Test_NexusWriter_2:
         assert re.search("#NEXUS", n)
         assert re.search("BEGIN DATA;", n)
         assert re.search("DIMENSIONS NTAX=3 NCHAR=2;", n)
+        assert re.search("MATRIX", n)
+        assert re.search("Latin\s+36", n)
+        assert re.search("French\s+14", n)
+        assert re.search("English\s+25", n)
+        assert re.search("FORMAT.*MISSING\=(.+?)", n).groups()[0] == '?'
+        assert re.search("FORMAT.*DATATYPE\=(\w+)\s", n).groups()[0] == 'STANDARD'
+        assert re.search('FORMAT.*SYMBOLS\="(\d+)";', n).groups()[0] == '123456'
+        
+    def test_nexus_charblock(self):
+        """Test Nexus Generation - with characters block"""
+        n = self.nex.make_nexus(charblock=True)
+        assert re.search("#NEXUS", n)
+        assert re.search("BEGIN DATA;", n)
+        assert re.search("DIMENSIONS NTAX=3 NCHAR=2;", n)
         assert re.search("CHARSTATELABELS", n)
         assert re.search("1 char1,", n)
         assert re.search("2 char2", n)
@@ -53,16 +67,13 @@ class Test_NexusWriter_2:
         assert re.search("FORMAT.*MISSING\=(.+?)", n).groups()[0] == '?'
         assert re.search("FORMAT.*DATATYPE\=(\w+)\s", n).groups()[0] == 'STANDARD'
         assert re.search('FORMAT.*SYMBOLS\="(\d+)";', n).groups()[0] == '123456'
-        
+    
     def test_nexus_interleave(self):
         """Test Nexus Generation - Interleaved"""
         n = self.nex.make_nexus(interleave=True)
         assert re.search("#NEXUS", n)
         assert re.search("BEGIN DATA;", n)
         assert re.search("DIMENSIONS NTAX=3 NCHAR=2;", n)
-        assert re.search("CHARSTATELABELS", n)
-        assert re.search("1 char1,", n)
-        assert re.search("2 char2", n)
         assert re.search("MATRIX", n)
         # char1
         assert re.search("Latin\s+3", n)
@@ -88,55 +99,50 @@ class Test_NexusWriter_Binary:
     
     def test_to_binary(self):
         """Test Nexus -> Binary: Two Character"""
-        n = copy.deepcopy(self.nex)
-        n.recode_to_binary()
+        self.nex.recode_to_binary()
         
-        assert n.data['char1']['French'] == '100'
-        assert n.data['char1']['English'] == '010'
-        assert n.data['char1']['Latin'] == '001'
+        assert self.nex.data['char1']['French'] == '100'
+        assert self.nex.data['char1']['English'] == '010'
+        assert self.nex.data['char1']['Latin'] == '001'
         
-        assert n.data['char2']['French'] == '100'
-        assert n.data['char2']['English'] == '010'
-        assert n.data['char2']['Latin'] == '001'
+        assert self.nex.data['char2']['French'] == '100'
+        assert self.nex.data['char2']['English'] == '010'
+        assert self.nex.data['char2']['Latin'] == '001'
         
     def test_to_binary_symbollist(self):
         """Test Nexus -> Binary: Update Symbol List"""
-        n = copy.deepcopy(self.nex)
-        n.recode_to_binary()
+        self.nex.recode_to_binary()
         
         # check symbol list was updated
-        assert len(n.symbols) == 2
-        assert '1' in n.symbols
-        assert '0' in n.symbols
+        assert len(self.nex.symbols) == 2
+        assert '1' in self.nex.symbols
+        assert '0' in self.nex.symbols
         
     def test_to_binary_combined(self):
         """Test Nexus -> Binary: Three Character, extra state"""
-        n = copy.deepcopy(self.nex)
         # add some more data...
-        n.add('French', 3, 'A')
-        n.add('Latin', 3, 'A')
-        n.add('English', 3, 'B')
-        n.recode_to_binary()
-        assert n.data['3']['Latin'] == '10'
-        assert n.data['3']['French'] == '10'
-        assert n.data['3']['English'] == '01'
+        self.nex.add('French', 3, 'A')
+        self.nex.add('Latin', 3, 'A')
+        self.nex.add('English', 3, 'B')
+        self.nex.recode_to_binary()
+        assert self.nex.data['3']['Latin'] == '10'
+        assert self.nex.data['3']['French'] == '10'
+        assert self.nex.data['3']['English'] == '01'
         
     def test_to_binary_missingdata(self):
         """Test Nexus -> Binary: Three Character, missing data"""
-        n = copy.deepcopy(self.nex)
         # add some more data...
-        n.add('French', 3, 'A')
-        n.add('Latin', 3, 'A')
+        self.nex.add('French', 3, 'A')
+        self.nex.add('Latin', 3, 'A')
         # no English state for char 3...
-        n.recode_to_binary()
-        assert re.search("English\s+\?", n.make_nexus())
+        self.nex.recode_to_binary()
+        assert re.search("English\s+\?", self.nex.make_nexus(interleave=True))
     
     @nose.tools.raises(AssertionError)
     def test_is_binary_dirtycheck(self):
         """Test Nexus -> Binary: is_binary dirty check"""
-        n = copy.deepcopy(self.nex)
-        n.recode_to_binary()
-        n.add('French', 4, 'A') # should raise AssertionError
+        self.nex.recode_to_binary()
+        self.nex.add('French', 4, 'A') # should raise AssertionError
         
 
 
