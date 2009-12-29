0b1f988dcd50ea794258cbecaaf4bd3c71f40777
Simon Greenhill <simon@simon.net.nz>
2009-12-30 00:10:29 +1300

    adding resample option

--- nexus/bin/nexus_randomise.py
+++ nexus/bin/nexus_randomise.py
@@ -6,33 +6,53 @@ __author__ = 'Simon Greenhill <simon@simon.net.nz>'
 
 import sys
 import os
-from random import shuffle
+from random import shuffle, randrange
 
 from nexus import NexusReader, NexusWriter
 
-def shufflenexus(nexus_obj):
+def shufflenexus(nexus_obj, resample=False):
     """
     Shuffles the characters between each taxon to create a new nexus
     
     :param nexus_obj: A `NexusReader` instance
     :type nexus_obj: NexusReader 
     
+    :param resample: The number of characters to resample. If set to False, then
+        the number of characters will equal the number of characters in the 
+        original data file.
+    :type resample: Integer
+    
     :return: A shuffled NexusReader instance
     :raises AssertionError: if nexus_obj is not a nexus
+    :raises ValueError: if resample is not False or a positive Integer
     :raises NexusFormatException: if nexus_obj does not have a `data` block
     """
     assert isinstance(nexus_obj, NexusReader), "Nexus_obj should be a NexusReader instance"
     if hasattr(nexus_obj, 'data') == False:
         raise NexusFormatException("Nexus has no `data` block")
     
+    if resample is False:
+        resample = nexus_obj.data.nchar
+    
+    try:
+        resample = int(resample)
+    except ValueError:
+        raise ValueError('resample must be a positive integer or False!')
+    
+    if resample < 1:
+        raise ValueError('resample must be a positive integer or False!')
+    
     newnexus = NexusWriter()
     newnexus.add_comment("Randomised Nexus generated from %s" % nexus_obj.filename)
-    for character in range(nexus_obj.data.nchar):
+    
+    for i in range(resample):
+        # pick existing character
+        character = randrange(0, nexus_obj.data.nchar)
         chars = nexus_obj.data.characters[character]
         site_values = [chars[taxon] for taxon in nexus_obj.data.taxa]
         shuffle(site_values)
         for taxon in nexus_obj.data.taxa:
-            newnexus.add(taxon, character, site_values.pop(0))
+            newnexus.add(taxon, i, site_values.pop(0))
     return newnexus
     
     
@@ -40,7 +60,10 @@ def shufflenexus(nexus_obj):
 if __name__ == '__main__':
     #set up command-line options
     from optparse import OptionParser
-    parser = OptionParser(usage="usage: %prog fudge.nex output.nex")
+    parser = OptionParser(usage="usage: %prog -n 100 fudge.nex output.nex")
+    parser.add_option("-n", "--numchars", dest="numchars", 
+            action="store", default=False, 
+            help="Number of Characters to Generate")
     options, args = parser.parse_args()
     
     try:
@@ -52,7 +75,14 @@ if __name__ == '__main__':
         parser.print_help()
         sys.exit()
         
+    if options.numchars != False:
+        try:
+            options.numchars = int(options.numchars)
+        except ValueError:
+            print "numchars needs to be a number!"
+            raise
+        
     nexus = NexusReader(nexusname)
-    nexus = shufflenexus(nexus)
+    nexus = shufflenexus(nexus, options.numchars)
     nexus.write_to_file(newnexus)
     print "New random nexus written to %s" % newnexus
--- nexus/test/test_bin.py
+++ nexus/test/test_bin.py
@@ -53,6 +53,9 @@ class Test_CountMissings:
 
 class Test_ShuffleNexus:
     
+    def setup(self):
+        self.nexus = NexusReader(os.path.join(EXAMPLE_DIR, 'example2.nex'))
+    
     @nose.tools.raises(AssertionError)
     def test_failure_on_nonnexus_1(self):
         shufflenexus({})
@@ -61,19 +64,29 @@ class Test_ShuffleNexus:
     def test_failure_on_nonnexus_2(self):
         shufflenexus("I AM NOT A NEXUS")
     
-    def test_constant(self):
-        # shuffling a constant character is not going to do anything...
-        nexus = NexusReader(os.path.join(EXAMPLE_DIR, 'example2.nex'))
-        nexus = shufflenexus(nexus)
-        for char, expected_value in {'0': 'a', '1': 'c', '2':'t', '3':'g'}.items():
-            for taxon, shuffled_value in nexus.data[char].items():
-                assert shuffled_value == expected_value
-    
     def test_output(self):
-        nexus = NexusReader(os.path.join(EXAMPLE_DIR, 'example2.nex'))
-        nexus = shufflenexus(nexus)
+        nexus = shufflenexus(self.nexus)
         assert isinstance(nexus, NexusWriter)
         
+    @nose.tools.raises(ValueError)
+    def test_resample_errorcheck_ok_1(self):
+        shufflenexus(self.nexus, "STRING")
+        
+    @nose.tools.raises(ValueError)
+    def test_resample_errorcheck_2(self):
+        shufflenexus(self.nexus, 0)
+        
+    def test_resample_10(self):
+        nexus = shufflenexus(self.nexus, 10)
+        assert len(nexus.characters) == 10
+        assert sorted(nexus.taxalist) == ['George', 'John', 'Paul', 'Ringo']
+
+    def test_resample_100(self):
+        nexus = shufflenexus(self.nexus, 100)
+        assert len(nexus.characters) == 100
+        assert sorted(nexus.taxalist) == ['George', 'John', 'Paul', 'Ringo']
+
+
 
 class Test_FindConstantSites:
     
