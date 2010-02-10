f51b639b72fe1600bca21be101fa3f4e25480a47
Simon J Greenhill <simon@simon.net.nz>
2010-02-10 17:22:54 +1300

    updating/refactoring tests

--- nexus/test/test_bin.py
+++ nexus/test/test_bin.py
@@ -10,7 +10,6 @@ from nexus.bin.nexus_remove_constantchars import find_constant_sites
 from nexus.bin.nexus_randomise import shufflenexus
 from nexus.bin.nexus_remove_uniquechars import find_unique_sites
 from nexus.bin.nexus_combine_nexus import combine_nexuses
-from nexus.bin.nexus_treemanip import run_deltree, run_resample, run_removecomments
 
 
 EXAMPLE_DIR = os.path.join(os.path.split(os.path.dirname(__file__))[0], 'examples')
@@ -240,63 +239,4 @@ class Test_CombineNexuses:
         assert re.search(r"""\bNCHAR=2\b""", newnex.write())
         assert re.search(r'\sSYMBOLS="12345"[\s;]', newnex.write())
         
-        
-class Test_TreeManip_run_deltree:
     
-    @nose.tools.raises(AssertionError)
-    def test_failure_on_nonnexus_1(self):
-        run_deltree('1', {}, do_print=False)
-        
-    @nose.tools.raises(AssertionError)
-    def test_failure_on_nonnexus_2(self):
-        run_deltree('1', "I AM NOT A NEXUS", do_print=False)
-    
-    def test_run_deltree(self):
-        #(options, nexus, do_print=True)
-        nexus = NexusReader(os.path.join(EXAMPLE_DIR, 'example.trees'))
-        new_nex = run_deltree('2', nexus, do_print=False)
-        assert len(new_nex.trees.trees) == 2
-        assert new_nex.trees.ntrees == 2
-        assert new_nex.trees[0].startswith('tree tree.0.1065.603220')
-        assert new_nex.trees[1].startswith('tree tree.20000.883.396049')
-    
-
-class Test_TreeManip_run_resample:
-
-    @nose.tools.raises(AssertionError)
-    def test_failure_on_nonnexus_1(self):
-        run_deltree('1', {}, do_print=False)
-        
-    @nose.tools.raises(AssertionError)
-    def test_failure_on_nonnexus_2(self):
-        run_deltree('1', "I AM NOT A NEXUS", do_print=False)
-        
-    def test_run_resample_1(self):
-        # shouldn't resample anything..
-        nexus = NexusReader(os.path.join(EXAMPLE_DIR, 'example.trees'))
-        new_nex = run_resample('1', nexus, do_print=False)
-        assert len(new_nex.trees.trees) == 3
-        assert new_nex.trees.ntrees == 3
-        assert new_nex.trees[0].startswith('tree tree.0.1065.603220')
-        assert new_nex.trees[1].startswith('tree tree.10000.874.808756')
-        assert new_nex.trees[2].startswith('tree tree.20000.883.396049')
-
-
-class Test_TreeManip_run_removecomments:
-    
-    @nose.tools.raises(AssertionError)
-    def test_failure_on_nonnexus_1(self):
-        run_removecomments({}, do_print=False)
-        
-    @nose.tools.raises(AssertionError)
-    def test_failure_on_nonnexus_2(self):
-        run_removecomments("I AM NOT A NEXUS", do_print=False)
-        
-    def test_run_removecomments(self):
-        # shouldn't resample anything..
-        nexus = NexusReader(os.path.join(EXAMPLE_DIR, 'example_beast.trees'))
-        new_nex = run_removecomments(nexus, do_print=False)
-        assert '[&lnP=-15795.47019648783]' not in new_nex.trees[0]
-        
-        
-        
