b00e9af45b0625310664531dcb2b9b9bcda5274d
Simon Greenhill <simon@simon.net.nz>
2010-05-24 12:49:00 +1200

    adding output argument for binary2multistate

--- nexus/bin/nexus_binary2multistate.py
+++ nexus/bin/nexus_binary2multistate.py
@@ -11,13 +11,21 @@ Converts binary nexuses to a multistate nexus.
 
 if __name__ == '__main__':
     from optparse import OptionParser
-    parser = OptionParser(usage="usage: %prog nex1.nex nex2.nex ... nexN.nex")
+    parser = OptionParser(usage="usage: %prog [-o output.nex] nex1.nex nex2.nex ... nexN.nex")
+    parser.add_option("-o", "--output", dest="output", 
+            action="store", default=None, type="string",
+            help="output nexus file")
     options, nexuslist = parser.parse_args()
-
+    
     if len(nexuslist) <= 1:
         print __doc__
         parser.print_help()
         sys.exit()
+        
+    if options.output is not None:
+        outfile = options.output
+    else:
+        outfile = 'multistate.nex'
     
     nexuslist2 = []
     for nfile in nexuslist:
@@ -26,5 +34,6 @@ if __name__ == '__main__':
         nexuslist2.append(n)
         
     out = combine_nexuses(nexuslist2)
-    out.write_to_file('multistate.nex', charblock=True, interleave=False)
-    print("Written to multistate.nex")
+    
+    out.write_to_file(outfile, charblock=True, interleave=False)
+    print("Written to %s" % outfile)
