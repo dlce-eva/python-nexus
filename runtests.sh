332c7be8cc86623a6358360e4658e43bf6a78412
Simon Greenhill <simon@simon.net.nz>
2011-11-02 21:17:05 +1300

    fixing tests

--- runtests.sh
+++ runtests.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-cd nexus/test
-for testfile in `ls -1 *.py`; do
-    echo $testfile
-    python $testfile
-done
-
-cd test_tools
-for testfile in `ls -1 *.py`; do
-    echo $testfile
-    python $testfile
-done
-
-cd ../..
+nosetests -w nexus/test/ $*
