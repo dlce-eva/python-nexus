2488064950b6f6adb81e22278ca09cf78191e7d8
Simon J Greenhill <simon@simon.net.nz>
2015-10-12 16:48:43 +1300

    adding test

---
+++ nexus/test/test_tools/test_tally_by_taxon.py
@@ -0,0 +1,45 @@
+import unittest
+
+from nexus import NexusReader
+from nexus.tools.sites import tally_by_taxon
+
+class Test_TallyByTaxon(unittest.TestCase):
+    """Test combine_nexuses"""
+    def setUp(self):
+        self.nex = NexusReader()
+        self.nex.read_string(
+            """Begin data;
+            Dimensions ntax=2 nchar=5;
+            Format datatype=standard symbols="12" gap=-;
+            Matrix
+            Harry              0111-?
+            Simon              0011-?
+            Elvis              0001-?
+            ;"""
+        )
+    
+    def test_errorcheck(self):
+        self.assertRaises(TypeError, tally_by_taxon, "I am a string")
+        self.assertRaises(TypeError, tally_by_taxon, 0)
+    
+    def test_tally_by_taxon(self):
+        tally = tally_by_taxon(self.nex)
+        # sites that are zero
+        assert tally['Harry']['0'] == [0]
+        assert tally['Simon']['0'] == [0, 1]
+        assert tally['Elvis']['0'] == [0, 1, 2]
+        
+        # sites that are 1
+        assert tally['Harry']['1'] == [1, 2, 3]
+        assert tally['Simon']['1'] == [2, 3]
+        assert tally['Elvis']['1'] == [3]
+        
+        # sites that are -
+        assert tally['Harry']['-'] == [4]
+        assert tally['Simon']['-'] == [4]
+        assert tally['Elvis']['-'] == [4]
+        
+        # sites that are ?
+        assert tally['Harry']['?'] == [5]
+        assert tally['Simon']['?'] == [5]
+        assert tally['Elvis']['?'] == [5]
