2fbfb8d5f35d94e3cc623fe15eee859cae6f95ae
SimonGreenhill <simon@simon.net.nz>
2015-10-21 10:11:52 +1300

    fixing test

--- nexus/test/test_handler_GenericHandler.py
+++ nexus/test/test_handler_GenericHandler.py
@@ -1,10 +1,32 @@
 """Tests for GenericHandler"""
+import os
 import unittest
-from nexus.reader import GenericHandler
+from nexus.reader import NexusReader, GenericHandler
+
+EXAMPLE_DIR = os.path.join(os.path.dirname(__file__), '../examples')
 
 class Test_GenericHandler(unittest.TestCase):
     def test_remove_comments(self):
         assert GenericHandler().remove_comments("bootstrap [bootstrap!]") == 'bootstrap '
+
+    def test_generic_readwrite(self):
+        expected = """Begin data;
+        Dimensions ntax=4 nchar=2;
+        Format datatype=standard symbols="01" gap=-;
+        Matrix
+        Harry              00
+        Simon              01
+        Betty              10
+        Louise             11
+        ;
+        """.split("\n")
+        nex = NexusReader()
+        nex.handlers['data'] = GenericHandler
+        nex.read_file(os.path.join(EXAMPLE_DIR, 'example.nex'))
+        for line in nex.data.write().split("\n"):
+            e = expected.pop(0).strip()
+            assert line.strip() == e
+
 
 if __name__ == '__main__':
     unittest.main()
--- nexus/test/test_reader.py
+++ nexus/test/test_reader.py
@@ -43,24 +43,6 @@ class Test_NexusReader_Core(unittest.TestCase):
         assert 'data' in nex.blocks
         assert 'Simon' in nex.blocks['data'].matrix
 
-    def test_generic_readwrite(self):
-        expected = """Begin data;
-        Dimensions ntax=4 nchar=2;
-        Format datatype=standard symbols="01" gap=-;
-        Matrix
-        Harry              00
-        Simon              01
-        Betty              10
-        Louise             11
-        ;
-        """.split("\n")
-        nex = NexusReader()
-        nex.handlers['data'] = GenericHandler
-        nex.read_file(os.path.join(EXAMPLE_DIR, 'example.nex'))
-        for line in nex.data.write().split("\n"):
-            e = expected.pop(0).strip()
-            assert line.strip() == e
-
     def test_write(self):
         nex = NexusReader(os.path.join(EXAMPLE_DIR, 'example.trees'))
         text = open(os.path.join(EXAMPLE_DIR, 'example.trees')).read()
