a3de360f3cb6865397d49d092633eed6fb937070
SimonGreenhill <simon@simon.net.nz>
2016-01-11 10:12:55 +1300

    adding some tests

--- nexus/test/test_tools/test_shufflenexus.py
+++ nexus/test/test_tools/test_shufflenexus.py
@@ -9,15 +9,24 @@ EXAMPLE_DIR = os.path.join(os.path.dirname(__file__), '../../examples')
 class Test_ShuffleNexus(unittest.TestCase):
     def setUp(self):
         self.nexus_obj = NexusReader(os.path.join(EXAMPLE_DIR, 'example2.nex'))
-
+    
     def test_output(self):
         nexus_obj = shufflenexus(self.nexus_obj)
         assert isinstance(nexus_obj, NexusWriter)
-
-    def test_resample_errorcheck_ok_1(self):
+    
+    def test_exception_resample_noninteger(self):
+        self.assertRaises(ValueError, shufflenexus, self.nexus_obj, 'a')
+        
+    def test_exception_resample_badinteger(self):
+        self.assertRaises(ValueError, shufflenexus, self.nexus_obj, 0)
+    
+    def test_exception_resample_negativeinteger(self):
+        self.assertRaises(ValueError, shufflenexus, self.nexus_obj, -1)
+    
+    def test_exception_nexus_obj_1(self):
         self.assertRaises(TypeError, shufflenexus, "I am a string")
-
-    def test_resample_errorcheck_2(self):
+    
+    def test_exception_nexus_obj_2(self):
         self.assertRaises(TypeError, shufflenexus, 0)
         
     def test_resample_1(self):
@@ -25,13 +34,13 @@ class Test_ShuffleNexus(unittest.TestCase):
         assert len(nexus_obj.characters) == 1
         assert sorted(nexus_obj.taxa) == \
             ['George', 'John', 'Paul', 'Ringo']
-
+    
     def test_resample_10(self):
         nexus_obj = shufflenexus(self.nexus_obj, 10)
         assert len(nexus_obj.characters) == 10
         assert sorted(nexus_obj.taxa) == \
             ['George', 'John', 'Paul', 'Ringo']
-
+    
     def test_resample_100(self):
         nexus_obj = shufflenexus(self.nexus_obj, 100)
         assert len(nexus_obj.characters) == 100
