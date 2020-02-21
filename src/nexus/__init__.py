eadc8e90c4a919a9ee8e790b4466b13844ea7d5b
xrotwang <xrotwang@googlemail.com>
2020-02-21 08:25:55 +0100

    bump version for development

--- src/nexus/__init__.py
+++ src/nexus/__init__.py
@@ -94,5 +94,5 @@ from nexus import handlers
 from nexus.exceptions import NexusFormatException
 from nexus import tools
 
-__version__ = "2.0.1"
+__version__ = "2.0.2.dev0"
 __all__ = ["NexusReader", "NexusWriter", "NexusFormatException", "handlers", "tools"]
