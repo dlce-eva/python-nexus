85cbc901f947fe2a8a443ab258450a7a9f7d61ab
Robert Forkel <xrotwang@googlemail.com>
2021-10-14 13:40:32 +0200

    release 2.2.0

--- src/nexus/__init__.py
+++ src/nexus/__init__.py
@@ -94,5 +94,5 @@ from nexus import handlers
 from nexus.exceptions import NexusFormatException
 from nexus import tools
 
-__version__ = "2.1.1.dev0"
+__version__ = "2.2.0"
 __all__ = ["NexusReader", "NexusWriter", "NexusFormatException", "handlers", "tools"]
