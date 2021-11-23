2099e058cd7394d691b540a321d8435574b07e12
Robert Forkel <xrotwang@googlemail.com>
2021-11-23 10:55:36 +0100

    bump version for development

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="python-nexus",
-    version="2.3.0",
+    version="2.3.1.dev0",
     description="A nexus (phylogenetics) file reader (.nex, .trees)",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
