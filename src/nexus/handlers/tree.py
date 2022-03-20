6272338bd969b26665ed5b12b01362fe4b702082
SimonGreenhill <simon@simon.net.nz>
2022-03-21 11:15:59 +1300

    fix another detranslate variant

--- src/nexus/handlers/tree.py
+++ src/nexus/handlers/tree.py
@@ -68,6 +68,7 @@ class TreeHandler(GenericHandler):
         (\[.+?\])?          # minimally match an optional comment chunk
         :?                  # optional colon
         (\[.+?\])           # minimally match an optional comment chunk
+        :?                  # optional colon
         (\d+(\.\d+)?)       # optional branchlengths
         (?=[),])?           # end boundary
     """, re.IGNORECASE + re.VERBOSE + re.DOTALL)
